4be78457674d37a4322380a8c07175be70c89164
Viktoria Henyk <viktoria.genuk@gmail.com>
2023-05-02 18:17:18 +0200

    Arrays-task1

---
+++ src/main/java/com/epam/rd/autotasks/array/IntArrayUtil.java
@@ -0,0 +1,52 @@
+package com.epam.rd.autotasks.array;
+
+import java.util.Arrays;
+
+public class IntArrayUtil {
+
+	public static void swapEven(int[] array) {
+
+		if (null != array) {
+			for (int i = 0; i < array.length / 2; i++) {
+				if ((array[i]) % 2 == 0 && array[array.length - 1 - i] % 2 == 0) {
+					int temp = array[i];
+					array[i] = array[array.length - 1 - i];
+					array[array.length - 1 - i] = temp;
+				}
+
+			}
+		}
+		else {
+			System.out.println("The array was null!");
+		}
+	}
+
+	public static void main(String[] args) {
+		{
+			int[] array = null;
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] {};
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] { 10, 5, 3, 4 };
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] { 100, 2, 3, 4, 5 };
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+		{
+			int[] array = new int[] { 100, 2, 3, 45, 33, 8, 4, 54 };
+			swapEven(array);
+			System.out.println(Arrays.toString(array));
+		}
+	}
+
+}
